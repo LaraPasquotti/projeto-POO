fa4f983b6ed2d8b9f6b6eabbf3a2772b6776a721
iwasmadeinyou <l176162@dac.unicamp.br>
2025-06-13 10:01:51 -0300

    adicionando a interface de filtro de pesquisa por avaliação e também os filtros por data e disciplina, mas ainda não implementados

---
+++ app/src/main/java/projetoPOO/AvaliacaoPorData.java
@@ -0,0 +1,14 @@
+package projetoPOO;
+
+import java.util.List;
+
+public class AvaliacaoPorData implements FiltroPesquisa {
+
+    @Override
+    public List<Avalicoes> meetCriteria(List<avalicoes> avaliacoes) {
+        
+    }
+
+
+
+}
---
+++ app/src/main/java/projetoPOO/AvaliacaoPorDisciplina.java
@@ -0,0 +1,12 @@
+package projetoPOO;
+
+import java.util.List;
+
+public class AvaliacaoPorDisciplina implements FiltroPesquisa {
+
+    @Override
+    public List<Avalicoes> meetCriteria(List<Avalicoes> avaliacoes) {
+        
+    }
+
+}
