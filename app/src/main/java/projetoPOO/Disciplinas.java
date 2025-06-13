f5666d61d7be0d0d559f7ba2b04c35a2637a7e68
iwasmadeinyou <l176162@dac.unicamp.br>
2025-06-13 09:50:51 -0300

    adicionando comentarios e os métodos de adicionar faltas, calcular media e remover faltas, mas ainda não implementados

--- app/src/main/java/projetoPOO/Disciplinas.java
+++ app/src/main/java/projetoPOO/Disciplinas.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * Contém a implementação da estrutura de uma classe Disciplina.
  * 
- * @author Lara Pasquotti, Isabel 
+ * @author Lara Pasquotti, Isabel Cristina e Lucas Nunes
  */
 public class Disciplinas {
 
@@ -15,7 +15,7 @@ public class Disciplinas {
     private ArrayList<Avaliacoes> avaliacoes;
 
     /**
-     * 
+     * Construtor da classe Disciplina
      * @param nomeDisciplina
      * @param numeroLimiteFaltas
      * @param faltasAtuais
@@ -23,39 +23,88 @@ public class Disciplinas {
     public Disciplinas(String nomeDisciplina, int numeroLimiteFaltas, int faltasAtuais){
         this.nomeDisciplina = nomeDisciplina;
         this.numeroLimiteFaltas = numeroLimiteFaltas;
-        this.faltasAtuais = faltasAtuais;
+        this.faltasAtuais = faltasAtuais; 
         this.avaliacoes = new ArrayList<>();
 
     }
 
+    /**
+     * Método de adicionar faltas
+     * @param faltasAtuais
+     */
+    public void adicionarFaltas(int faltasAtuais){
+
+    }
+
+    public void calcularMedia(){
+
+    }
+
+    
+    public void removerFaltas(){
+
+    }
+
+    /**
+     * Retorna o nome da disciplina.
+     * @return o nome da disciplina
+     */
     public String getNomeDisciplina() {
         return nomeDisciplina;
     }
 
+    /**
+     * Altera o nome da disciplina.
+     * @param nomeDisciplina o novo nome da disciplina
+     */
     public void setNomeDisciplina(String nomeDisciplina) {
         this.nomeDisciplina = nomeDisciplina;
     }
 
+    /**
+     * Retoena o número limites de faltas da disciplina
+     * @return o número limeites de faltas
+     */
     public int getNumeroLimiteFaltas() {
         return numeroLimiteFaltas;
     }
 
+    /**
+     * Altera o numero limite de faltas da disciplina
+     * @param numeroLimiteFaltas o novo numero limtite de faltas
+     */
     public void setNumeroLimiteFaltas(int numeroLimiteFaltas) {
         this.numeroLimiteFaltas = numeroLimiteFaltas;
     }
 
+    /**
+     * Retorna as faltas atuais do aluno
+     * @return as faltas atuais do aluno
+     */
     public int getFaltasAtuais() {
         return faltasAtuais;
     }
 
+    /**
+     * Altera as faltas atuais do aluno
+     * @param faltasAtuais as novas faltas atuais do aluno
+     */
     public void setFaltasAtuais(int faltasAtuais) {
         this.faltasAtuais = faltasAtuais;
     }
 
+    /**
+     * 
+     * @return
+     */
     public ArrayList<Avaliacoes> getAvaliacoes() {
         return avaliacoes;
     }
 
+    /**
+     * 
+     * @param avaliacoes
+     */
     public void setAvaliacoes(ArrayList<Avaliacoes> avaliacoes) {
         this.avaliacoes = avaliacoes;
     }
