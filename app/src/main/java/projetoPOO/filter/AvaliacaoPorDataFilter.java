6d1a86314ad2cd168f85b2a8856e743a7c178782
Isabel Cristina Marras Salles <i188790@dac.unicamp.br>
2025-06-16 16:26:57 -0300

    organização pasta filter e renomeação para padrão filter

--- app/src/main/java/projetoPOO/filter/AvaliacaoPorDataFilter.java
+++ app/src/main/java/projetoPOO/filter/AvaliacaoPorDataFilter.java
@@ -1,14 +1,16 @@
-package projetoPOO.model;
+package projetoPOO.filter;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import projetoPOO.model.Avaliacoes;
 
 /**
  * Contém a estrutura de implementação de um filtro por data.
  * 
  * @author Lara Pasquotti, Isabel Cristina e Lucas Nunes
  */
-public class AvaliacaoPorData implements FiltroPesquisa {
+public class AvaliacaoPorDataFilter implements Filter {
 
     @Override
     public List<Avaliacoes> meetCriteria(List<Avaliacoes> avaliacoes) {
--- app/src/main/java/projetoPOO/filter/Filter.java
+++ app/src/main/java/projetoPOO/filter/Filter.java
@@ -1,13 +1,14 @@
-package projetoPOO.model;
+package projetoPOO.filter;
 
 import java.util.List;
+import projetoPOO.model.*;
 
 /**
  * Contém a estrutura de implementação de uma interface de filtro de pesquisa para as avaliações.
  * 
  * @author Lara Pasquotti, Isabel Cristina e Lucas Nunes
  */
-public interface FiltroPesquisa {
+public interface Filter {
 
     /**
      * Contrutor da interface de filtro de pesquisas.
