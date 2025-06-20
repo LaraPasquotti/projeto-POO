7f753b68222f9862f72b7e747f45f95545cb43c2
iwasmadeinyou <l176162@dac.unicamp.br>
2025-06-20 16:36:04 -0300

    arrumei uns detalhes da tela e comecei o main controller

--- app/src/main/java/projetoPOO/ui/MainController.java
+++ app/src/main/java/projetoPOO/ui/MainController.java
@@ -1,5 +1,13 @@
 package projetoPOO.ui;
+import javafx.fxml.Initializable;
+import java.util.ResourceBundle;
+import java.net.URL;
 
-public class MainController {
+public class MainController implements Initializable {
+
+    @Override
+    public void initialize(URL uri, ResourceBundle rb){
+        
+    }
     
 }
