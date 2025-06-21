bd94b1d24988d095d4e4e92c6bbd6c16f7bc293d
iwasmadeinyou <l176162@dac.unicamp.br>
2025-06-21 13:57:10 -0300

    comecei a testar o funcionamento da tela de login e de autenticar usuario

--- app/src/main/java/projetoPOO/ui/MainApp.java
+++ app/src/main/java/projetoPOO/ui/MainApp.java
@@ -2,12 +2,16 @@ package projetoPOO.ui;
 
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
+    /** 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
@@ -18,6 +22,28 @@ public class MainApp extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
+    } */
+
+    @FXML
+    private TextField email;
+    private TextField senha;
+
+    
+    @Override
+    public void start(Stage loginStage) throws Exception{
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/TelaLogin.fxml"));
+        Scene scene = new Scene(loader.load(), 335, 600);
+        loginStage.setTitle("Tela Login");
+        loginStage.setScene(scene);
+        loginStage.show();
+
+    }
+
+    @FXML
+    public void onBotaoLogin(ActionEvent e){
+        String emailDigitado = email.getText();
+        String senhaDigitada = senha.getText();
     }
 
     public static void main(String[] args) {
