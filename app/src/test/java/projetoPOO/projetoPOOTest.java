218b2203337564daa9f358edea4b758ab1d738e0
Lara <larapasquotti07@gmail.com>
2025-06-22 12:12:42 -0300

    Tirando imports nao usados

--- app/src/test/java/projetoPOO/projetoPOOTest.java
+++ app/src/test/java/projetoPOO/projetoPOOTest.java
@@ -1,17 +1,5 @@
 package projetoPOO;
 
-import org.junit.jupiter.api.Test;
-
-import projetoPOO.model.*;
-import projetoPOO.exceptions.*;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-
 public class projetoPOOTest {
 
 }
