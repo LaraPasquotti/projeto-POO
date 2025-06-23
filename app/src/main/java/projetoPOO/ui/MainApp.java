98e09328035ed21c707f90905354c1cc975e9697
Isabel Cristina Marras Salles <i188790@dac.unicamp.br>
2025-06-23 16:36:57 -0300

    correções em MainApp

--- app/src/main/java/projetoPOO/ui/MainApp.java
+++ app/src/main/java/projetoPOO/ui/MainApp.java
@@ -10,19 +10,6 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
-    /** 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/TelaVerDisciplinas.fxml"));
-        Scene scene = new Scene(loader.load(), 335, 600);
-        primaryStage.setTitle("teste");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-    }*/
-
     @Override
     public void start(Stage loginStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/TelaLogin.fxml"));
